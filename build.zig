be39e5033d56af49798121b6c7a754a556335c1d
Jonathan Marler <johnnymarler@gmail.com>
2021-04-15 17:07:36 -0600

    Address https://github.com/ziglang/zig/issues/8544

--- build.zig
+++ build.zig
@@ -16,6 +16,12 @@ pub fn build(b: *std.build.Builder) void {
         exe.setBuildMode(mode);
         exe.linkLibC();
         exe.install();
+        // NOTE: these should be added with the pragmas inside the source file
+        //       but pragmas aren't working for cross-compilation
+        //           https://github.com/ziglang/zig/issues/8544
+        exe.linkSystemLibrary("user32");
+        exe.linkSystemLibrary("gdi32");
+        exe.linkSystemLibrary("ws2_32");
     }
     {
         const exe = b.addExecutable("wrc-server", null);
@@ -27,5 +33,9 @@ pub fn build(b: *std.build.Builder) void {
         exe.setBuildMode(mode);
         exe.linkLibC();
         exe.install();
+        // NOTE: these should be added with the pragmas inside the source file
+        //       but pragmas aren't working for cross-compilation
+        //           https://github.com/ziglang/zig/issues/8544
+        exe.linkSystemLibrary("ws2_32");
     }
 }
